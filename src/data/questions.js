e11c6a64c0a4c769742b0a31b0b85c4ac3c1e8f7
j.korzeniewski <j.korzeniewski@millionmonkeys.pl>
2024-04-05 00:05:27 +0300

    Updates

--- src/data/questions.js
+++
@@ -1,9 +0,0 @@
-export const QUESTIONS = [
-    "Nocuje u Ciebie XYZ. Wracasz wcześniej niż planowałeś i nakrywasz go z XYZ1 w swoim łóżku - co robisz?",
-    "Jesteś XYZ1 i jedziesz autobusem. Wsiada kanar i orientujesz się, że nie masz biletu - co robisz?",
-    "Jesteś XYZ1 i przespałeś budzik na ważne spotkanie, wstajesz 15 minut przed nim - co robisz?",
-    "XYZ mówi, że ma pomysł na świetny biznes i potrzebuje, żebyś zainwestował swoje oszczędności - co robisz?",
-    "Jesteś XYZ1 i właśnie wróciłeś ze sklepu z nowym wazonem i XYZ Ci go stłukł wychodząc z łazienki - co mówisz?",
-    "Jesteś XYZ1 i właśnie stłukłeś XYZ2 nowy wazon - co mówisz?",
-    "Jesteś XYZ1 i Tobie oraz XYZ2 spodobała się ta sama dziewczyna - co robisz?"
-];
